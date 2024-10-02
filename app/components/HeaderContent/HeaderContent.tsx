1478c661c689d010c9b834fb716d065e2673df97
TakashiNishii <takashi@geeklys.com>
2024-10-02 14:03:28 -0300

    :art: Hero section

--- app/components/HeaderContent/HeaderContent.tsx
+++ app/components/HeaderContent/HeaderContent.tsx
@@ -5,7 +5,7 @@ const HeaderContent = () => {
   return (
     <div className='hidden md:flex md:flex-row'>
       <Image src="/images/Logo.png" alt="Logo AZtec" width="150" height="150" />
-      <nav className="flex w-full text-secondary font-semibold justify-center items-center mr-36">
+      <nav className="flex w-full text-secondary font-semibold justify-center items-center lg:mr-36">
         <ul className="flex flex-row">
           <li className="p-2">Home</li>
           <li className="p-2">Quem Somos?</li>
---
+++ app/components/Hero.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div
+      className="hero max-h-[500px] bg-[url('/images/imageHero.png')] bg-cover bg-center"
+    >
+      <div className="hero-overlay bg-primary bg-opacity-75"></div>
+      <div className="hero-content text-neutral-content text-center">
+        <div className="max-w-md flex flex-col items-center gap-5">
+          <Image src="/images/LogoHero.png" width={360} height={150} alt="logo" />
+          <h2 className="text-2xl text-secondary font-bold">
+            Soluções integradas de engenharia
+            e manutenção agroindustrial
+          </h2>
+          <button className="mt-3 btn btn-secondary btn-circle w-full text-primary text-xl font-bold">Saiba Mais</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
