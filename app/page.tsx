c2bf5e065f5d2d277a0f09192061e8b544add942
TakashiNishii <takashi@geeklys.com>
2024-10-02 14:42:55 -0300

    :rocket: 'Quem somos' section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import QuemSomos from "./components/QuemSomos";
 
 export default function Home() {
   return (
     <div className="flex flex-col">
       <Header />
       <Hero />
+      <QuemSomos />
     </div>
   );
 }
