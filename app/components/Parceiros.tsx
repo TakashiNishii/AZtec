079d1821d15f338fc3ebfa2d8430774210de8f12
TakashiNishii <takashi@geeklys.com>
2024-10-08 10:26:41 -0300

    :rocket: Parceiros

--- app/components/Parceiros.tsx
+++ app/components/Parceiros.tsx
@@ -1,22 +1,27 @@
 import React from 'react'
+import ParceirosList from './ParceirosContent/ParceirosList'
 
 const Parceiros = () => {
   return (
-    <div
-      className="hero min-h-[300px] bg-[url('/images/parceiros.png')] bg-cover bg-center"
-    >
-      <div className="hero-overlay bg-primary bg-opacity-75"></div>
+    <>
+      <div
+        className="hero min-h-[300px] bg-[url('/images/parceiros.png')] bg-cover bg-center"
+      >
+        <div className="hero-overlay bg-primary bg-opacity-75"></div>
 
-      <div className="hero-content w-full md:w-1/4 flex-col gap-2 text-neutral-content text-center">
-        <h2 className="text-2xl text-secondary font-bold">
-          Nossos Parceiros
-        </h2>
-        <div className='divider divider-secondary w-full my-0 md:min-w-md' />
-        <p className='text-base text-secondary font-semibold'>
-          Após anos no mercado, estabelecemos parcerias sólidas com empresas que depositam total confiança em nossos serviços, tais como:
-        </p>
+        <div className="hero-content w-full md:w-1/4 flex-col gap-2 text-neutral-content text-center">
+          <h2 className="text-2xl text-secondary font-bold">
+            Nossos Parceiros
+          </h2>
+          <div className='divider divider-secondary w-full my-0 md:min-w-md' />
+          <p className='text-base text-secondary font-semibold'>
+            Após anos no mercado, estabelecemos parcerias sólidas com empresas que depositam total confiança em nossos serviços, tais como:
+          </p>
+        </div>
       </div>
-    </div>
+      <ParceirosList />
+    </>
+
   )
 }
 
---
+++ app/components/ParceirosContent/ParceirosList.tsx
@@ -0,0 +1,58 @@
+"use client"
+import React from "react"
+import { useKeenSlider } from "keen-slider/react"
+import "keen-slider/keen-slider.min.css"
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const animation = { duration: 90000, easing: (t: any) => t }
+
+const ParceirosList = () => {
+  const [sliderRef] = useKeenSlider({
+    loop: true,
+    renderMode: "performance",
+    drag: false,
+    created(s) {
+      s.moveToIdx(5, true, animation)
+    },
+    updated(s) {
+      s.moveToIdx(s.track.details.abs + 5, true, animation)
+    },
+    animationEnded(s) {
+      s.moveToIdx(s.track.details.abs + 5, true, animation)
+    },
+  })
+  // array of 200 positions
+  const array = Array.from({ length: 20 }, (_, i) => i + 1)
+  return (
+    <div ref={sliderRef} className="keen-slider">
+      {array.map(() => (
+        <>
+          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro1.png')] bg-no-repeat h-[150px]  !max-w-[25%] !min-w-[25%]" />
+          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro2.png')] bg-no-repeat h-[150px]  !max-w-[25%] !min-w-[25%]" />
+          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro3.png')] bg-no-repeat h-[150px]  !max-w-[25%] !min-w-[25%]" />
+          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro4.png')] bg-no-repeat h-[150px]  !max-w-[25%] !min-w-[25%]" />
+          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro5.png')] bg-no-repeat h-[150px]  !max-w-[25%] !min-w-[25%]" />
+          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro6.png')] bg-no-repeat h-[150px]  !max-w-[25%] !min-w-[25%]" />
+          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro7.png')] bg-no-repeat h-[150px]  !max-w-[25%] !min-w-[25%]" />
+          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro8.png')] bg-no-repeat h-[150px]  !max-w-[25%] !min-w-[25%]" />
+          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro9.png')] bg-no-repeat h-[150px]  !max-w-[25%] !min-w-[25%]" />
+          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro10.png')] bg-no-repeat h-[150px]  !max-w-[25%] !min-w-[25%]" />
+          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro11.png')] bg-no-repeat h-[150px]  !max-w-[25%] !min-w-[25%]" />
+          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro12.png')] bg-no-repeat h-[150px]  !max-w-[25%] !min-w-[25%]" />
+          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro13.png')] bg-no-repeat h-[150px]  !max-w-[25%] !min-w-[25%]" />
+          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro14.png')] bg-no-repeat h-[150px]  !max-w-[25%] !min-w-[25%]" />
+          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro15.png')] bg-no-repeat h-[150px]  !max-w-[25%] !min-w-[25%]" />
+          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro16.png')] bg-no-repeat h-[150px]  !max-w-[25%] !min-w-[25%]" />
+          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro17.png')] bg-no-repeat h-[150px]  !max-w-[25%] !min-w-[25%]" />
+          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro18.png')] bg-no-repeat h-[150px]  !max-w-[25%] !min-w-[25%]" />
+          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro19.png')] bg-no-repeat h-[150px]  !max-w-[25%] !min-w-[25%]" />
+          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro20.png')] bg-no-repeat h-[150px]  !max-w-[25%] !min-w-[25%]" />
+          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro21.png')] bg-no-repeat h-[150px]  !max-w-[25%] !min-w-[25%]" />
+          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro22.png')] bg-no-repeat h-[150px]  !max-w-[25%] !min-w-[25%]" />
+        </>
+      ))}
+    </div>
+  )
+}
+
+export default ParceirosList
