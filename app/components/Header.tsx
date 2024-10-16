a8b9973deed96317011d99d828ff7c228a68d452
TakashiNishii <takashi@geeklys.com>
2024-10-16 15:30:35 -0300

    :art: Adjust header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,7 +5,7 @@ import HeaderContentMobile from "./HeaderContent/HeaderContentMobile"
 
 export const Header = () => {
   return (
-    <div className="w-full bg-primary shadow-md z-30">
+    <div className="w-full bg-primary shadow-xl border-b-4 border-blue-900 z-30">
       <HeaderContent />
       <HeaderContentMobile />
     </div>
