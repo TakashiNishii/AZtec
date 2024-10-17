a6f356865d9edb38bcaa6cf3ea9fbf98a343fae7
TakashiNishii <takashi@geeklys.com>
2024-10-17 12:17:25 -0300

    :art: Padronização de imagem de parceiros

--- app/components/ParceirosContent/ParceirosListMobile.tsx
+++ app/components/ParceirosContent/ParceirosListMobile.tsx
@@ -27,28 +27,28 @@ const ParceirosListMobile = () => {
     <div ref={sliderRef} className="keen-slider md:!hidden">
       {array.map(() => (
         <>
-          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro1.png')] bg-no-repeat h-[150px]  md:!max-w-[25%] md:!min-w-[25%]" />
-          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro2.png')] bg-no-repeat h-[150px]  md:!max-w-[25%] md:!min-w-[25%]" />
-          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro3.png')] bg-no-repeat h-[150px]  md:!max-w-[25%] md:!min-w-[25%]" />
-          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro4.png')] bg-no-repeat h-[150px]  md:!max-w-[25%] md:!min-w-[25%]" />
-          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro5.png')] bg-no-repeat h-[150px]  md:!max-w-[25%] md:!min-w-[25%]" />
-          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro6.png')] bg-no-repeat h-[150px]  md:!max-w-[25%] md:!min-w-[25%]" />
-          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro7.png')] bg-no-repeat h-[150px]  md:!max-w-[25%] md:!min-w-[25%]" />
-          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro8.png')] bg-no-repeat h-[150px]  md:!max-w-[25%] md:!min-w-[25%]" />
-          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro9.png')] bg-no-repeat h-[150px]  md:!max-w-[25%] md:!min-w-[25%]" />
-          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro10.png')] bg-no-repeat h-[150px]  md:!max-w-[25%] md:!min-w-[25%]" />
-          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro11.png')] bg-no-repeat h-[150px]  md:!max-w-[25%] md:!min-w-[25%]" />
-          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro12.png')] bg-no-repeat h-[150px]  md:!max-w-[25%] md:!min-w-[25%]" />
-          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro13.png')] bg-no-repeat h-[150px]  md:!max-w-[25%] md:!min-w-[25%]" />
-          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro14.png')] bg-no-repeat h-[150px]  md:!max-w-[25%] md:!min-w-[25%]" />
-          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro15.png')] bg-no-repeat h-[150px]  md:!max-w-[25%] md:!min-w-[25%]" />
-          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro16.png')] bg-no-repeat h-[150px]  md:!max-w-[25%] md:!min-w-[25%]" />
-          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro17.png')] bg-no-repeat h-[150px]  md:!max-w-[25%] md:!min-w-[25%]" />
-          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro18.png')] bg-no-repeat h-[150px]  md:!max-w-[25%] md:!min-w-[25%]" />
-          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro19.png')] bg-no-repeat h-[150px]  md:!max-w-[25%] md:!min-w-[25%]" />
-          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro20.png')] bg-no-repeat h-[150px]  md:!max-w-[25%] md:!min-w-[25%]" />
-          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro21.png')] bg-no-repeat h-[150px]  md:!max-w-[25%] md:!min-w-[25%]" />
-          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro22.png')] bg-no-repeat h-[150px]  md:!max-w-[25%] md:!min-w-[25%]" />
+          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro1.png')] bg-no-repeat h-[150px]  md:!max-w-[15%] md:!min-w-[15%]" />
+          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro2.png')] bg-no-repeat h-[150px]  md:!max-w-[15%] md:!min-w-[15%]" />
+          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro3.png')] bg-no-repeat h-[150px]  md:!max-w-[15%] md:!min-w-[15%]" />
+          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro4.png')] bg-no-repeat h-[150px]  md:!max-w-[15%] md:!min-w-[15%]" />
+          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro5.png')] bg-no-repeat h-[150px]  md:!max-w-[15%] md:!min-w-[15%]" />
+          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro6.png')] bg-no-repeat h-[150px]  md:!max-w-[15%] md:!min-w-[15%]" />
+          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro7.png')] bg-no-repeat h-[150px]  md:!max-w-[15%] md:!min-w-[15%]" />
+          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro8.png')] bg-no-repeat h-[150px]  md:!max-w-[15%] md:!min-w-[15%]" />
+          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro9.png')] bg-no-repeat h-[150px]  md:!max-w-[15%] md:!min-w-[15%]" />
+          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro10.png')] bg-no-repeat h-[150px]  md:!max-w-[15%] md:!min-w-[15%]" />
+          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro11.png')] bg-no-repeat h-[150px]  md:!max-w-[15%] md:!min-w-[15%]" />
+          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro12.png')] bg-no-repeat h-[150px]  md:!max-w-[15%] md:!min-w-[15%]" />
+          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro13.png')] bg-no-repeat h-[150px]  md:!max-w-[15%] md:!min-w-[15%]" />
+          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro14.png')] bg-no-repeat h-[150px]  md:!max-w-[15%] md:!min-w-[15%]" />
+          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro15.png')] bg-no-repeat h-[150px]  md:!max-w-[15%] md:!min-w-[15%]" />
+          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro16.png')] bg-no-repeat h-[150px]  md:!max-w-[15%] md:!min-w-[15%]" />
+          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro17.png')] bg-no-repeat h-[150px]  md:!max-w-[15%] md:!min-w-[15%]" />
+          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro18.png')] bg-no-repeat h-[150px]  md:!max-w-[15%] md:!min-w-[15%]" />
+          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro19.png')] bg-no-repeat h-[150px]  md:!max-w-[15%] md:!min-w-[15%]" />
+          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro20.png')] bg-no-repeat h-[150px]  md:!max-w-[15%] md:!min-w-[15%]" />
+          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro21.png')] bg-no-repeat h-[150px]  md:!max-w-[15%] md:!min-w-[15%]" />
+          <div className="keen-slider__slide bg-[url('/images/parceiros/parceiro22.png')] bg-no-repeat h-[150px]  md:!max-w-[15%] md:!min-w-[15%]" />
         </>
       ))}
     </div>
